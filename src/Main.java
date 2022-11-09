8de3efc5684abf7823361c2b92aed116b76858a3
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-11-09 18:47:54 +0500

    Решение задачи 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 1
+        int personId = 1024;
+        byte personAge = 34;
+        short counter = 5234;
+        long countOfStars = 84723894728934L;
+        float posX = 0.27f;
+        double posY = 1.0d;
+
+        System.out.println("Значение переменной personId с типом int равно " + personId);
+        System.out.println("Значение переменной personAge с типом byte равно " + personAge);
+        System.out.println("Значение переменной counter с типом short равно " + counter);
+        System.out.println("Значение переменной countOfStars с типом long равно " + countOfStars);
+        System.out.println("Значение переменной posX с типом float равно " + posX);
+        System.out.println("Значение переменной posY с типом double равно " + posY);
+
     }
 }
